9dbb6634d52015df48747f261b35352c01f0b647
Trung Tran <trandinhtrung176@gmail.com>
2025-05-04 20:46:01 +0700

    refactor: update Vercel adapter configuration in astro.config.mjs
    - Replaced the Vercel serverless adapter with the new Vercel adapter.
    - Configured incremental static regeneration (ISR) with a bypass token and specified paths for fresh content delivery.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercelServerless from "@astrojs/vercel/serverless";
+
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 import vue from "@astrojs/vue";
@@ -8,8 +9,17 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercelServerless({
-    edgeMiddleware: true,
+  adapter: vercel({
+    isr: {
+      // A secret random string that you create.
+      bypassToken: "005556d774a8",
+      // Paths that will always be served fresh.
+      exclude: [
+        "/",
+        "/blog/[page]",
+        /^\/api\/.+/, // Regular expressions supported since @astrojs/vercel@v8.1.0
+      ],
+    },
   }),
   vite: {
     plugins: [tailwindcss()],
