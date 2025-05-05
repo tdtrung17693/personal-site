f78cb8ab44f36014cacac4a3a3db4ce766b91025
Trung Tran <trandinhtrung176@gmail.com>
2025-05-05 07:31:50 +0700

    refactor: update Vercel serverless adapter configuration in astro.config.mjs
    - Replaced the Vercel adapter with the serverless version.
    - Enabled edge middleware for improved performance and routing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel";
+import vercelServerless from "@astrojs/vercel/serverless";
 import tailwindcss from "@tailwindcss/vite";
 
 import vue from "@astrojs/vue";
@@ -9,17 +8,8 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    isr: {
-      // A secret random string that you create.
-      bypassToken: "005556d774a8",
-      // Paths that will always be served fresh.
-      exclude: [
-        "/",
-        "/blog/[page]",
-        /^\/api\/.+/, // Regular expressions supported since @astrojs/vercel@v8.1.0
-      ],
-    },
+  adapter: vercelServerless({
+    edgeMiddleware: true,
   }),
   vite: {
     plugins: [tailwindcss()],
